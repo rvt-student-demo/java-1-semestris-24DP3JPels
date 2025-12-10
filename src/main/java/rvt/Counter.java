9245acd3437a00d6ec4fe65358621281c737a7e2
24DP3JPels <A240529JP@rvt.lv>
2025-12-10 15:39:18 +0200

    Counter no java mooc 5.2 par overload counter

---
+++ src/main/java/rvt/Counter.java
@@ -0,0 +1,41 @@
+package rvt;
+
+public class Counter {
+    
+    private int count;
+
+
+    public Counter(int startValue) {
+        this.count = startValue;
+    }
+
+    public Counter() {
+        this.count = 0;
+    }
+
+
+    public int value() {
+        return this.count;
+    }
+
+    public void increase() {
+        this.count++;
+    }
+
+    public void decrease() {
+        this.count--;
+    }
+    
+    public void increase(int increaseBy) {
+        if (increaseBy >= 0) {
+            this.count += increaseBy; 
+        }
+    }
+
+    
+    public void decrease(int decreaseBy) {
+        if (decreaseBy >= 0) {
+            this.count -= decreaseBy; 
+        }
+    }
+}
